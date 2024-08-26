db8d1fc682e1fb6e5bd87662ff8b03e9de21fbbb
Titouan Real <titouan.real@gmail.com>
2024-08-26 13:06:34 +0200

    Use `const` declarations in lib documentation example

--- src/lib.rs
+++ src/lib.rs
@@ -16,32 +16,35 @@
 //! An example using CSIDH-512:
 //!
 //! ```
-//! use csidh::{CsidhParams, PrivateKey, PublicKey, SharedSecret};
+//! use csidh::{csidh_512, CsidhParams, PrivateKey, PrivateKeyCsidh512, PublicKey, SharedSecret};
 //!
-//! let params = CsidhParams::CSIDH_512;
 //! let mut rng = rand::thread_rng();
 //!
 //! // Alice
-//! let alice_path = [
-//!     8, 2, 9, 3, 3, 0, 7, 2, 0, 8, 1, 9, 9, 4, 0, 10, 6, 3, 10, 7, 2, 3, 1, 4, 5, 3, 9, 10, 9,
-//!     3, 8, 5, 1, 10, 2, 4, 2, 10, 1, 1, 10, 8, 0, 9, 1, 8, 7, 6, 10, 9, 9, 4, 10, 6, 4, 4, 2,
-//!     3, 5, 5, 5, 3, 0, 9, 6, 9, 8, 5, 5, 9, 2, 0, 3, 6,
-//! ];
-//! let alice_private_key = PrivateKey::new(params, alice_path);
-//! let alice_public_key = PublicKey::from(alice_private_key, &mut rng);
+//! const ALICE_PRIVATE_KEY: PrivateKeyCsidh512 = PrivateKey::new(
+//!     CsidhParams::CSIDH_512,
+//!     [
+//!         8, 2, 9, 3, 3, 0, 7, 2, 0, 8, 1, 9, 9, 4, 0, 10, 6, 3, 10, 7, 2, 3, 1, 4, 5, 3, 9, 10,
+//!         9, 3, 8, 5, 1, 10, 2, 4, 2, 10, 1, 1, 10, 8, 0, 9, 1, 8, 7, 6, 10, 9, 9, 4, 10, 6, 4,
+//!         4, 2, 3, 5, 5, 5, 3, 0, 9, 6, 9, 8, 5, 5, 9, 2, 0, 3, 6,
+//!     ],
+//! );
+//! let alice_public_key = PublicKey::from(ALICE_PRIVATE_KEY, &mut rng);
 //!
 //! // Bob
-//! let bob_path = [
-//!     1, 2, 0, 6, 2, 1, 2, 6, 4, 3, 10, 1, 4, 0, 1, 7, 5, 6, 9, 10, 8, 9, 7, 5, 4, 7, 10, 10, 5,
-//!     6, 5, 2, 1, 4, 0, 6, 0, 3, 8, 7, 0, 10, 0, 3, 0, 3, 6, 9, 2, 3, 4, 4, 3, 3, 0, 10, 10, 2,
-//!     1, 4, 8, 10, 6, 0, 7, 1, 2, 7, 2, 0, 9, 9, 0, 6,
-//! ];
-//! let bob_private_key = PrivateKey::new(params, bob_path);
-//! let bob_public_key = PublicKey::from(bob_private_key, &mut rng);
+//! const BOB_PRIVATE_KEY: PrivateKeyCsidh512 = PrivateKey::new(
+//!     CsidhParams::CSIDH_512,
+//!     [
+//!         1, 2, 0, 6, 2, 1, 2, 6, 4, 3, 10, 1, 4, 0, 1, 7, 5, 6, 9, 10, 8, 9, 7, 5, 4, 7, 10, 10,
+//!         5, 6, 5, 2, 1, 4, 0, 6, 0, 3, 8, 7, 0, 10, 0, 3, 0, 3, 6, 9, 2, 3, 4, 4, 3, 3, 0, 10,
+//!         10, 2, 1, 4, 8, 10, 6, 0, 7, 1, 2, 7, 2, 0, 9, 9, 0, 6,
+//!     ],
+//! );
+//! let bob_public_key = PublicKey::from(BOB_PRIVATE_KEY, &mut rng);
 //!
 //! // Shared secret
-//! let alice_shared_secret = SharedSecret::from(bob_public_key, alice_private_key, &mut rng);
-//! let bob_shared_secret = SharedSecret::from(alice_public_key, bob_private_key, &mut rng);
+//! let alice_shared_secret = SharedSecret::from(bob_public_key, ALICE_PRIVATE_KEY, &mut rng);
+//! let bob_shared_secret = SharedSecret::from(alice_public_key, BOB_PRIVATE_KEY, &mut rng);
 //! assert_eq!(alice_shared_secret, bob_shared_secret);
 //! ```
 
